dd92160a90ec10d6c27c965e2459343bd95b406d
Vasyl Latushko <vasyl.latushko@gmail.com>
2025-10-03 14:21:17 +0300

    Adds a simple Cloud Run resource

--- spacelift/gcp_integration/main.tf
+++ spacelift/gcp_integration/main.tf
@@ -17,3 +17,21 @@ data "google_project" "project" {
 output "project_id" {
   value = data.google_project.project.project_id
 }
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
